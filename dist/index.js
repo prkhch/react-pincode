6c0d2305140b1d7a80d5670029436d47a6fefa40
PARKHEECHANG <shjc4623@gmail.com>
2023-12-15 01:47:17 +0900

    chore: publish npm

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Test", {
+  enumerable: true,
+  get: function () {
+    return _test.default;
+  }
+});
+var _test = _interopRequireDefault(require("./test"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
